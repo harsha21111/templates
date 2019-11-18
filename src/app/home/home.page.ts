da9e814ddd04a7ad87fbe99d75655fd2fc3b19cb
harshavardhan <harshavardan2k17@gmail.com>
2019-11-18 12:59:30 +0530

    searchbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public searchControl: FormControl;
+  public items: any;
 
-  constructor() {}
+  constructor(private dataService: DataService) {
+    this.searchControl = new FormControl();
+  }
 
+  ngOnInit() {
+    this.setFilteredItems('');
+
+    this.searchControl.valueChanges
+      .pipe(debounceTime(700))
+      .subscribe(search => {
+        this.setFilteredItems(search);
+      });
+  }
+
+  setFilteredItems(searchTerm) {
+    this.items = this.dataService.filterItems(searchTerm);
+  }
 }
